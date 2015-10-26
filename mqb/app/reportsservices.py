7a70a0be6d599d4aa58269e27a7c2539080fac1d
Salvador Ruiz <sruiz@ako.com>
2015-10-26 13:53:36 +0100

    Created pdf file object as output

--- mqb/app/reportsservices.py
+++ mqb/app/reportsservices.py
@@ -1,22 +1,68 @@
-import latex
+import subprocess
+import tempfile
 import jinja2
 import os
-#import pdb
+import pdb
+from shutil import rmtree
 
 print(os.getcwd())
 
 
 class ReportGenerator(object):
 
-    def __init__(self, template_path):
+    def __init__(self, template_path, working_path):
         template_loader = jinja2.FileSystemLoader(searchpath=template_path)
         self.env = jinja2.Environment(loader=template_loader)
-        #pdb.set_trace()
+        self.working_path = working_path
+        self.template_path = template_path
+        # pdb.set_trace()
 
-    def printToPDF(self, template, variables=None):
-
+    def parse_tex_file(self, template, variables=None):
         template = self.env.get_template(template)
-        self._parsed_tex = template.render(variables)
-        pdf = latex.build_pdf(self._parsed_tex)
+        return template.render(variables)
 
-        return pdf
+    def print_to_PDF(self, tex_content):
+        temp_dir = tempfile.mkdtemp(dir=self.template_path)
+
+        tex_file = tempfile.TemporaryFile(mode="w+t",
+                                          dir=temp_dir, suffix=".tex",
+                                          delete=False)
+        tex_file.write(tex_content)
+        tex_file.close()
+
+        base_filename = os.path.splitext(tex_file.name)[0]
+        pdf_filename = base_filename + '.pdf'
+
+        latex_cmd = ['pdflatex',
+                     '-interaction=batchmode',
+                     '-halt-on-error',
+                     '-no-shell-escape',
+                     '-file-line-error',
+                     '%O',
+                     '%S',
+                     ]
+
+        latex_cmd = ['latexmk', '-pdf',
+                                '-pdflatex={}'.format(' '.join(latex_cmd)),
+                                tex_file.name, ]
+
+        # create environment
+        newenv = os.environ.copy()
+        # newenv['TEXINPUTS'] = os.pathsep.join(texinputs) + os.pathsep
+
+        try:
+            subprocess.check_call(
+                args=latex_cmd,
+                cwd=temp_dir,
+                env=newenv,
+                stdin=open(os.devnull, 'r'),
+                stdout=open(os.devnull, 'w'),
+                stderr=open(os.devnull, 'w'),
+            )
+        except subprocess.CalledProcessError as e:
+            print(e)
+
+        # pdb.set_trace()
+
+        yield open(pdf_filename, 'rb')
+        rmtree(temp_dir)
--- mqb/test/test_ReportService.py
+++ mqb/test/test_ReportService.py
@@ -8,23 +8,30 @@ class TestReportGenerator(unittest.TestCase):
     templates_path = os.getcwd()
     templates_path = os.path.join(templates_path, "mqb",
                                   "test", "templates")
+    pdf_directory = os.path.join(templates_path, "pdfOutput")
+
+    def test_texfile_is_parsed(self):
+        template = "helloVariable.tex"
+        name = "Salvador"
+        var = {"name": name}
+
+        report_generator = ReportGenerator(TestReportGenerator.templates_path,
+                                           TestReportGenerator.pdf_directory)
+        tex_document = report_generator.parse_tex_file(template, var)
+        self.assertTrue(tex_document.find(name))
 
     def test_generate_something(self):
         template = 'hello.tex'
         var = {}
-        report_generator = ReportGenerator(TestReportGenerator.templates_path)
-        pdf = report_generator.printToPDF(template, var)
+        report_generator = ReportGenerator(TestReportGenerator.templates_path,
+                                           TestReportGenerator.pdf_directory)
+        tex_document = report_generator.parse_tex_file(template, var)
+        pdf = report_generator.print_to_PDF(tex_document)
+
         assert pdf
 
-    def test_variables_are_replaced(self):
-        template = "helloVariable.tex"
-        name = "Salvador"
-        var = {"name": name}
-        report_generator = ReportGenerator(TestReportGenerator.templates_path)
-        pdf = report_generator.printToPDF(template, var)
-        print(report_generator._parsed_tex)
-        assert pdf
-        assert report_generator._parsed_tex.find(name)
+    def test_generate_pdf_with_images(self):
+        pass
 
 
 if __name__ == '__main__':
